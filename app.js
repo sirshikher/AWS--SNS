7a84dd3b99ca741c0045923e683d1c1ece571f5c
shikher <sirshikher@gmail.com>
2020-04-05 15:42:37 +0530

    intial setup

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-const express = require('express');
+import express from "express";
+import dotenv from 'dotenv';
+import AWS from 'aws-sdk';
+
+dotenv.config();
 const app = express();
-require('dotenv').config();
-
-var AWS = require('aws-sdk');
-
-app.get('/', (req, res) => {
-
+app.get("/", (req, res) => {
     console.log("Message = " + req.query.message);
     console.log("Number = " + req.query.number);
     console.log("Subject = " + req.query.subject);
+    var AWSconfig = new AWS.SNS({ apiVersion: "2010-03-31" });
     var params = {
-        Message: req.query.message,
-        PhoneNumber: '+' + req.query.number,
+        Message: process.env.VERIFY_PORT + `type=phone/code=${req.query.message}/value=${req.query.number}`,
+        PhoneNumber: "+" + req.query.number,
         MessageAttributes: {
-            'AWS.SNS.SMS.SenderID': {
-                'DataType': 'String',
-                'StringValue': req.query.subject
+            "AWS.SNS.SMS.SenderID": {
+                DataType: "String",
+                StringValue: req.query.subject
             }
         }
     };
+    var SMSAttributes = {
+        attributes: {
+            DefaultSMSType: "Transactional"
+        }
+    };
+    AWSconfig.setSMSAttributes(SMSAttributes);
 
-    var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
-
-    publishTextPromise.then(
-        function (data) {
+    var publishTextPromise = AWSconfig.publish(params).promise();
+    publishTextPromise
+        .then(function (data) {
             res.end(JSON.stringify({ MessageID: data.MessageId }));
-        }).catch(
-            function (err) {
-                res.end(JSON.stringify({ Error: err }));
-            });
-
+        })
+        .catch(function (err) {
+            res.end(JSON.stringify({ Error: err }));
+        });
 });
-
-app.listen(3000, () => console.log('SMS Service Listening on PORT 3000'))
+app.listen(3002, () => console.log("SMS Service Listening on PORT 3002"));
